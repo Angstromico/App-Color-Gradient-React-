f9deabe2945fcde8a5c7f56cd85c8343ba3cf4dd
kervisvasquez1993 <kervisvasquez22@gmail.com>
2021-12-05 20:35:02 -0400

    Color Gradient Add fix bad things

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SingleColor from './SingleColor';
 import Values from 'values.js';
 
 function App() {
-  const [color, setColor] = useState('');
+  const [color, setColor] = useState('#FF6F61');
   const [error, setError] = useState(false);
   const [gradientNumber, setGradientNumber] = useState(20);
   const [blackWhite, setBlackWhite] = useState(5);
