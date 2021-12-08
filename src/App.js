f42c5d3d9787f3ed9bb5131dababb3911fbb0e63
kervisvasquez1993 <kervisvasquez22@gmail.com>
2021-12-07 20:28:39 -0400

    Solve Fail on Chose Button

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [error, setError] = useState(false);
   const [gradientNumber, setGradientNumber] = useState(20);
   const [blackWhite, setBlackWhite] = useState(5);
-  const [list, setList] = useState(new Values(color).all(gradientNumber));
+  const [list, setList] = useState(new Values('#FF6F61').all(gradientNumber));
   const [liStyles, setLiStyles] = useState(false);
   const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
   const year = new Date().getFullYear();
